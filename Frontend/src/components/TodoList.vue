40b0d70ebf155719800754fb8e10a1e23bc80c3c
mercybassey <udohmercy911@gmail.com>
2023-10-11 23:55:28 +0100

    Minore

--- Frontend/src/components/TodoList.vue
+++ Frontend/src/components/TodoList.vue
@@ -1,16 +1,24 @@
 <template>
-  <div class="todo-container">
-    <div class="todo-list">
-      <h1>Todo List</h1>
-      <div class="todo-form">
-        <input class="todo-input" v-model="newTodo.title" placeholder="Add new todo title" />
-        <input class="todo-input" v-model="newTodo.description" placeholder="Add description" />
-        <input type="checkbox" v-model="newTodo.completed" /> Completed
-        <input type="date" v-model="newTodo.dueDate" />
-        <button class="btn btn-add" @click="addTodo">Add Todo</button>
-      </div>
+  <div>
+    <div v-if="error" class="error-message">
+      {{ error }}
     </div>
-    <div class="todo-items">
+
+    <div v-else class="container">
+      <div class="todo-container">
+        <div class="todo-list">
+          <h1>Todo List</h1>
+          <div class="todo-form">
+            <input class="todo-input" v-model="newTodo.title" placeholder="Add new todo title" />
+            <input class="todo-input" v-model="newTodo.description" placeholder="Add description" />
+            <input type="checkbox" v-model="newTodo.completed" /> Completed
+            <input type="date" v-model="newTodo.dueDate" />
+            <button class="btn btn-add" @click="addTodo">Add Todo</button>
+          </div>
+        </div>
+      </div>
+
+      <div class="todo-items">
       <table>
         <thead>
           <tr>
@@ -41,8 +49,10 @@
         </tbody>
       </table>
     </div>
+    </div> 
   </div>
 </template>
+
 
 <script setup>
   import { ref } from 'vue';
@@ -55,51 +65,71 @@
     completed: false,
     dueDate: null
   });
+  const error = ref();
 
   const fetchTodos = async () => {
-    const response = await axios.get('http://localhost:3000/todos');
-    todos.value = response.data;
+    try {
+      const response = await axios.get('http://localhost:3000/todos');
+      todos.value = response.data;
+    } catch (err) { 
+      error.value = err.response?.data?.message || "An error occurred while fetching todos.";
+    }
   };
 
   const addTodo = async () => {
-    const response = await axios.post('http://localhost:3000/todos_create', newTodo.value);
-    todos.value.push(response.data);
-    newTodo.value = {
-      title: '',
-      description: '',
-      completed: false,
-      dueDate: null
-    };
+    try {
+      const response = await axios.post('http://localhost:3000/todos_create', newTodo.value);
+      todos.value.push(response.data);
+      newTodo.value = {
+        title: '',
+        description: '',
+        completed: false,
+        dueDate: null
+      };
+    } catch (err) {
+      error.value = err.response?.data?.message || "An error occurred while adding the todo.";
+    }
   };
 
   const deleteTodo = async (id) => {
-    await axios.delete(`http://localhost:3000/todos/${id}`);
-    todos.value = todos.value.filter(todo => todo._id !== id);
+    try {
+      await axios.delete(`http://localhost:3000/todos/${id}`);
+      todos.value = todos.value.filter(todo => todo._id !== id);
+    } catch (err) {
+      error.value = err.response?.data?.message || "An error occurred while deleting the todo.";
+    }
   };
 
   const updateTodo = async (todo) => {
-    // Prompt the user to update each field
-    const newTitle = prompt('Update todo title:', todo.title);
-    const newDescription = prompt('Update todo description:', todo.description);
-    const newDueDate = prompt('Update todo due date:', todo.dueDate ? new Date(todo.dueDate).toLocaleDateString() : '');
-    const newCompleted = confirm('Is the todo completed? Click "OK" for Yes and "Cancel" for No.');
+    try {
+      const newTitle = prompt('Update todo title:', todo.title);
+      const newDescription = prompt('Update todo description:', todo.description);
+      const newDueDate = prompt('Update todo due date:', todo.dueDate ? new Date(todo.dueDate).toLocaleDateString() : '');
+      const newCompleted = confirm('Is the todo completed? Click "OK" for Yes and "Cancel" for No.');
 
-    // Create an updated todo object
-    const updatedTodo = {
-      ...todo,
-      title: newTitle !== null ? newTitle : todo.title,
-      description: newDescription !== null ? newDescription : todo.description,
-      dueDate: newDueDate !== null ? new Date(newDueDate).toISOString() : todo.dueDate,
-      completed: newCompleted
-    };
+      const updatedTodo = {
+        ...todo,
+        title: newTitle !== null ? newTitle : todo.title,
+        description: newDescription !== null ? newDescription : todo.description,
+        dueDate: newDueDate !== null ? new Date(newDueDate).toISOString() : todo.dueDate,
+        completed: newCompleted
+      };
 
-    // Send the updated todo to the server
-    const response = await axios.put(`http://localhost:3000/todos/${todo._id}`, updatedTodo);
-
-    // Update the local state
-    const index = todos.value.findIndex(t => t._id === todo._id);
-    todos.value[index] = response.data;
+      const response = await axios.put(`http://localhost:3000/todos/${todo._id}`, updatedTodo);
+      const index = todos.value.findIndex(t => t._id === todo._id);
+      todos.value[index] = response.data;
+    } catch (err) {
+      error.value = err.response?.data?.message || "An error occurred while updating the todo.";
+    }
   };
 
   fetchTodos();
 </script>
+
+<style scoped>
+  .error-message {
+    color: red;
+    font-weight: bold;
+    margin-bottom: 20px;
+  }
+</style>
